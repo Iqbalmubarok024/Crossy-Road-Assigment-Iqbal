9db2109661a60249c8e98229afa79e93c4c52332
Iqbalmubarok24 <mubarokiqbal362@gmail.com>
2023-04-27 11:41:03 +0700

    random-Grass-Tree

--- Assets/Terrain.cs
+++ Assets/Terrain.cs
@@ -22,11 +22,28 @@ public class Terrain : MonoBehaviour
 
         for (int i = -limit ; i <= limit ; i++)
         {
+            SpawnTile(i);
+        }
+        var leftBoundaryTile = SpawnTile(-limit -1);
+        var rightBoundaryTile = SpawnTile(limit +1);
+        DarkenObject(leftBoundaryTile);
+        DarkenObject(rightBoundaryTile);
+    }
+    private GameObject SpawnTile (int xPos)
+    {
             var go = Instantiate(tilePrefab, transform);
 
-            go.transform.localPosition = new Vector3(i,0,0);
-        }
+            go.transform.localPosition = new Vector3(xPos,0,0);
 
-       
+            return go;
+    }
+    
+    private void DarkenObject(GameObject go)
+    {
+        var renderers = go.GetComponentsInChildren<MeshRenderer>(includeInactive: true);
+        foreach (var rend in renderers)
+        {
+            rend.material.color = rend.material.color * Color.grey;
+        }
     }
 }
